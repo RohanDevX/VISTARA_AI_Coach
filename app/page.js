5bd5f4f7c1fe990cb99c1a9392731d1ad5db4460
RohanDevX <152317528+RohanDevX@users.noreply.github.com>
2025-06-14 20:35:30 +0530

    WIP: local changes before rebase

--- app/page.js
+++ app/page.js
@@ -3,13 +3,5 @@ import { Ghost } from "lucide-react";
 import Link from "next/link";
 
 export default function Home() {
-  return <div>hello there, this is vistara
-    <br/>
-   <Button >
-    hello
-   </Button>
-
-
-
-  </div>;
+  return 
 }
